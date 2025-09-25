1a8c482f234f2275fe0721bc98b0a65fcf360140
Esther Wandera <wanderaesther809@gmail.com>
2025-09-26 00:02:53 +0300

    first commit

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
 }
+
+
+// Select the button element
+const button = document.getElementById('button');
+
+// Function that adds an event listener
+function addingEventListener() {
+  button.addEventListener('click', () => {
+    console.log("Button clicked!");
+  });
+}
